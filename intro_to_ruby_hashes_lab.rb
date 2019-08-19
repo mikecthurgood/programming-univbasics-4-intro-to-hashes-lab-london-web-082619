1771e5bab5ca0e3a8bef33b47be7e2f48d31fecd
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-19 14:59:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,5 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] = nil
-    hash[key] = 1
-  else
-    hash[key] += 1
-  end
+  if hash[key].nil? hash[key] = 1 : hash[key] += 1
 end
